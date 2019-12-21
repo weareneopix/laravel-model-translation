e569263a0339671b48dedf7a5adaecaa672f4bd5
Nguyễn Trần Chung <nguyentranchung52th@gmail.com>
2019-12-21 20:38:24 +0900

    fix #2

--- src/Jobs/SyncModelLanguageMapping.php
+++ src/Jobs/SyncModelLanguageMapping.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use WeAreNeopix\LaravelModelTranslation\Translation;
 
 class SyncModelLanguageMapping implements ShouldQueue
 {
